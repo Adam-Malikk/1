d20f2b30285847768747f0bbfe97005c5994c89b
Adam <adammalikq01@gmail.com>
2024-10-26 00:19:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,153 @@
+// // // let hewan = 'jerapah'
+// // // let naweh = hewan.split('')
+// // // console.log(naweh)
+
+// // // let angka = [2, 3, 4, 5, 6, 7]
+// // // let biodata = [
+// // //     {nama : 'adam'},
+// // //     {nama : 'malik'},
+// // //     {nama : 'asep'}
+// // // ]
+// // // const data = ['hallo', 'selamat', 'datang']
+
+// // // let angkas = angka.filter((angkas) => {
+// // //     if(angkas % 2 == 0){
+// // //         return true
+// // //     }
+// // // })
+// // // console.log(angkas)
+
+// // // let num = angka.forEach((x) => {
+// // //     let j = x * 2
+// // //     if(j >= 2){
+// // //         console.log(j)
+// // //     }
+// // // })
+
+// // // let y = angka.reduce((angka1, angka2) => angka1 + angka2, 0)
+// // // console.log(y)
+
+
+// // // let b = data.reduce((a1, a2) => a1 + ' ' + a2)
+// // // console.log(b)
+
+// // // let h = biodata.filter(x => x.nama.startsWith('a'))
+// // // console.log(h)
+
+// // // const fruits = [
+// // //         [
+// // //             ["Banana"],
+// // //             [ "Orange"],
+// // //         ],
+// // //         "Apple", "Mango"];
+// // // let fruit = fruits.at(1); //let fruit = fruits[1]
+// // // let text = fruits.constructor
+// // // console.log(fruits.copyWithin(3, 1))
+// // // console.log(fruit)
+// // // console.log(text)
+
+// // // // let fruit = fruits.entries()
+// // // // let text = "";
+// // // for (let x of fruit) {
+// // //   text += x;
+// // // }
+// // // console.log(fruit)
+
+// // // // let fruit = fruits.reverse()
+// // // console.log(fruits)
+// // // console.log(fruit)
+// // // console.log(fruits.toString())
+// // // let v = fruits.filter
+
+// // // // fruits.forEach((vallue, index) => {
+// // // //     console.log(`${index} : ${vallue}`)
+// // // // });
+
+// // // let barang = []
+
+// // // function tugas() {
+// // //     let command = ''
+
+// // //     while(command !== 'keluar'){
+// // //         command = prompt('Masukkan perintah (tambah, hapus, lihat, keluar):').toLowerCase()
+
+// // //         if(command === 'tambah'){
+// // //             let tambah = prompt('barang apa yang akan ditambah?')
+// // //             let menambah = barang.push()
+// // //             // let h = 
+// // //         }
+// // //     }
+// // // }
+
+// // // tugas()
+
+// // // let d = 'index'
+// // // console.log(d)
+
+// // // for (let index = array.length; index > -1; index--) {
+// // //     result.push(array[index])
+// // //     result.pop()
+// // // }
+
+// // // console.log(result)
+
+
+// // // let array = ['1', '2', '3', '4']
+// // // let result = []
+
+// // // for (let i = 0; i < result.length; i++) {
+// // //     result.push(parseInt(result[i]))
+// // // }
+
+// // // for (let index = array.length - 1; index >= 0; index--) {
+// // //     result.push(array[index])
+// // // }
+
+// // // for (let i = 0; i < result.length; i++) {
+// // //     let u = parseInt(result[i])
+// // //     result[i] = u
+// // // }
+
+// // // for (let n = 0; n < result.length; n++){
+// // //     result[n] *= 8
+// // // }
+
+// // // for (let i = 0; i < result.length; i++) {
+// // //     result[i] *= 4
+// // //     if(result[i] < 100) {
+// // //         continue
+// // //     }else{
+// // //         break
+// // //     }
+// // // }
+
+// // // for (let i = 0; i < result.length; i++) {
+// // //     result[i] *= 4
+// // //     if(result[i] % 6 == 0) {
+// // //         console.log(true)
+// // //     }else{
+// // //         continue
+// // //     }
+// // // }
+
+// // // console.log(result)
+
+// // function getValue() {
+// //     const value = document.getElementById('myInput').value;
+// //     console.log(value); // Output: Hello World
+// //   }
+
+// // getValue()
+
+// let txt = 'adam malik'
+// let txt1 = 'malik'
+
+// let txt2 = txt.concat(txt1)
+// let txt3 = txt2.split()
+// console.log(txt3)
+
+const t = 'HAPPY BIRTHDAY SAYANGGGGGGGGG'
+
+for(i = 0; i < 1000; i++){
+    console.log(t)
+}
